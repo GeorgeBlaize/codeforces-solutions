4dbb7c4903eeab272025e31ad7974f35d9033fad
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-05-08 22:26:37 +0600

    Add files via upload

---
+++ Group Contests/Sheet #1 (Data type - Conditions)/Y_The_last_2_digits.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+
+
+    long long a,b,c,d;
+
+    cin>>a>>b>>c>>d;
+
+    long long result=(a%100);
+
+    result=(result*(b%100))%100;
+    result=(result*(c%100))%100;
+    result=(result*(d%100))%100;
+    
+    if(result<10){
+        cout<<"0"<<result<<endl;
+    }
+    else{
+        cout<<result<<endl;
+    }
+    
+
+
+    return 0;
+}
