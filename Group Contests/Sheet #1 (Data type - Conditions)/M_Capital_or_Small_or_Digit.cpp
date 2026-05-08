54aa14dccce4c30322b74f92dc0d8bc71a54050a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-05-08 20:02:24 +0600

    Add files via upload

---
+++ Group Contests/Sheet #1 (Data type - Conditions)/M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    char x;
+
+    cin>>x;
+
+    if(x>='a' && x<='z'){
+        cout<<"ALPHA"<<endl;
+        cout<<"IS SMALL"<<endl;
+    }
+    else if(x>='A' && x<='A'){
+        cout<<"ALPHA"<<endl;
+        cout<<"IS CAPITAL"<<endl;
+    }
+    else{
+        cout<<"IS DIGIT"<<endl;
+    }
+
+
+    return 0;
+}
