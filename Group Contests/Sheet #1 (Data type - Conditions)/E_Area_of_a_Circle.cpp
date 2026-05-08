32dbe05b88109497e3cb384f1b9aa24eded655ae
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-05-08 19:15:07 +0600

    Add files via upload

---
+++ Group Contests/Sheet #1 (Data type - Conditions)/E_Area_of_a_Circle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    double PI,r,area;
+    
+    PI=3.141592653;
+
+    cin>>r;
+
+    area=PI*(r*r);
+
+    cout<<fixed<<setprecision(9)<<area<<endl;
+    
+    return 0;
+}
